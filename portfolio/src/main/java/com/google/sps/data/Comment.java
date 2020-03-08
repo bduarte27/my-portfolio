f7155c5ef67468d82b13ce3a0b6a81cf5b87a4d2
Brian Duarte <duartebrian27@gmail.com>
2020-03-08 04:38:09 -0700

    Add super smooth comment section updates, add styles to blog, and refactor to clean java code

---
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -0,0 +1,41 @@
+package com.google.sps.data;
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.gson.annotations.Expose;
+import javax.servlet.http.HttpServletRequest;
+
+public class Comment {
+  public static final String ENTITY = "Comment";
+  private static final String AUTHOR_PROPERTY = "author";
+  private static final String COMMENT_PROPERTY = "comment";
+  private static final String TIMESTAMP_PROPERTY = "timestamp";
+
+  @Expose private String author;
+  @Expose private String comment;
+  @Expose private long timestamp;
+
+  private Comment(String author, String comment, long timestamp) {
+    this.author = author;
+    this.comment = comment;
+    this.timestamp = timestamp;
+  }
+
+  public static Comment fromEntity(Entity entity) {
+    return new Comment((String) entity.getProperty(AUTHOR_PROPERTY),
+        (String) entity.getProperty(COMMENT_PROPERTY),
+        (long) entity.getProperty(TIMESTAMP_PROPERTY));
+  }
+
+  public static Comment fromServletRequest(HttpServletRequest request) {
+    return new Comment(request.getParameter(AUTHOR_PROPERTY),
+        request.getParameter(COMMENT_PROPERTY), System.currentTimeMillis());
+  }
+
+  public Entity toEntity() {
+    Entity entity = new Entity(ENTITY);
+    entity.setProperty(AUTHOR_PROPERTY, author);
+    entity.setProperty(COMMENT_PROPERTY, comment);
+    entity.setProperty(TIMESTAMP_PROPERTY, timestamp);
+    return entity;
+  }
+}
